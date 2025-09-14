c40315f2f610d54aa90657385510789ec77f3f74
lobomanz <lovro.monkey@gmail.com>
2025-09-14 12:52:34 +0200

    first commit cms

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using GoatSilencerArchitecture.Models;
+
+namespace GoatSilencerArchitecture.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<Gallery> Galleries { get; set; }
+        public DbSet<GalleryImage> GalleryImages { get; set; }
+    }
+}
