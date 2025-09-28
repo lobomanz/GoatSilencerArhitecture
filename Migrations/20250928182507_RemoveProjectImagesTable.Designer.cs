b95dee580ac2574c6d84552730e3102431ab79e4
lobomanz <lovro.monkey@gmail.com>
2025-09-28 20:26:05 +0200

    uopdated database

--- Migrations/20250928182507_RemoveProjectImagesTable.Designer.cs
+++ Migrations/20250928182507_RemoveProjectImagesTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using GoatSilencerArchitecture.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GoatSilencerArchitecture.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250928182507_RemoveProjectImagesTable")]
+    partial class RemoveProjectImagesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.8");
---
+++ Migrations/20250928182507_RemoveProjectImagesTable.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GoatSilencerArchitecture.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveProjectImagesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImageModel_Blogs_BlogComponentId",
+                table: "ImageModel");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_ImageModel_Projects_ProjectId",
+                table: "ImageModel");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ImageModel",
+                table: "ImageModel");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ImageModel_ProjectId",
+                table: "ImageModel");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectId",
+                table: "ImageModel");
+
+            migrationBuilder.RenameTable(
+                name: "ImageModel",
+                newName: "BlogImages");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ImageModel_BlogComponentId",
+                table: "BlogImages",
+                newName: "IX_BlogImages_BlogComponentId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_BlogImages",
+                table: "BlogImages",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BlogImages_Blogs_BlogComponentId",
+                table: "BlogImages",
+                column: "BlogComponentId",
+                principalTable: "Blogs",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BlogImages_Blogs_BlogComponentId",
+                table: "BlogImages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_BlogImages",
+                table: "BlogImages");
+
+            migrationBuilder.RenameTable(
+                name: "BlogImages",
+                newName: "ImageModel");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_BlogImages_BlogComponentId",
+                table: "ImageModel",
+                newName: "IX_ImageModel_BlogComponentId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "ProjectId",
+                table: "ImageModel",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ImageModel",
+                table: "ImageModel",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ImageModel_ProjectId",
+                table: "ImageModel",
+                column: "ProjectId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImageModel_Blogs_BlogComponentId",
+                table: "ImageModel",
+                column: "BlogComponentId",
+                principalTable: "Blogs",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ImageModel_Projects_ProjectId",
+                table: "ImageModel",
+                column: "ProjectId",
+                principalTable: "Projects",
+                principalColumn: "Id");
+        }
+    }
+}
